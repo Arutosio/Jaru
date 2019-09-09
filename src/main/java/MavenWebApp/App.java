926bacce75005bcd006a9108f5f4e1b0933e9c47
Arutosio <Arutosio@outlook.com>
2019-09-10 01:05:44 +0200

    simple hello Aru

--- src/main/java/MavenWebApp/App.java
+++ src/main/java/MavenWebApp/App.java
@@ -1,6 +1,8 @@
 package MavenWebApp;
 
 /**
+ * Run project command:
+ * C:\\Users\\stefa\\.vscode\\extensions\\vscjava.vscode-java-debug-0.21.0\\scripts\\launcher.bat "C:\\Program Files\\Java\\jdk-11.0.1\\bin\\java" -Dfile.encoding=UTF-8 -cp C:\\Users\\stefa\\Documents\\GitHub\\Maven-WebApp\\target\\classes MavenWebApp.App
  * Hello world!
  *
  */
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String x = "Arutosio";
+        System.out.println( "Hello World! " + x );
     }
 }
